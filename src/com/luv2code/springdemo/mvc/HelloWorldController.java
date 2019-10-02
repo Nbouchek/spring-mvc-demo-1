a88e49c66970e998c056b55442f97250364a1cc4
Nacer Bouchekhima <nbouchek@gmail.com>
2019-10-02 17:15:15 +0100

    Spring MVC @RequestParam demo.

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -32,6 +33,20 @@ public class HelloWorldController {
 
         // create message
         String result = "Yo! => " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+        // convert data to all caps
+        theName = theName.toUpperCase();
+
+        // create message
+        String result = "Hey friend from v3! " + theName;
 
         // add message to the model
         model.addAttribute("message", result);
